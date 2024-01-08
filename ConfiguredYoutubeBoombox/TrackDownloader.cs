a84b999323ad00692a6bbdfc613f34b8bbb3c79f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-08 10:46:05 +0000

    parse timestamps and use effective duration to check if a track is too long
    closes #2

--- ConfiguredYoutubeBoombox/TrackDownloader.cs
+++ ConfiguredYoutubeBoombox/TrackDownloader.cs
@@ -11,6 +11,8 @@ namespace ConfiguredYoutubeBoombox;
 
 public class TrackDownloader
 {
+    private static readonly int[] timestampPartMagnitudes = [1, 60, 3600];
+    
     protected static async Task<float> FetchSongDuration(string id)
     {
         Logger?.LogDebug("Fetching video metadata.");
@@ -22,6 +24,72 @@ public class TrackDownloader
         }
 
         throw new Exception("Failed to fetch video duration.");
+    }
+    
+    /// <summary>
+    /// Parse an ffmpeg timestamp string to a number of seconds.
+    /// </summary>
+    /// <param name="timestamp">The timestamp to parse</param>
+    /// <returns>The timestamp's time in seconds</returns>
+    protected static float ParseTimestamp(string timestamp)
+    {
+        if (String.IsNullOrWhiteSpace(timestamp))
+            throw new ArgumentException("Timestamp cannot be null or whitespace");
+        
+        if (float.TryParse(timestamp, out var parsedFloat)) return parsedFloat;
+
+        var parts = timestamp.Split(":")
+            .Select(part => part.Trim())
+            .Reverse()
+            .Select((part, index) => (part, index))
+            .ToArray();
+
+        if (parts.Length >= 3)
+            throw new NotImplementedException("Timestamp has more than 3 parts, support is not implemented");
+
+        try
+        {
+            var seconds = float.Parse(parts[0].part);
+            
+            foreach (var (part, index) in parts[1..])
+            {
+                if (part is null) continue;
+                seconds += int.Parse(part) * timestampPartMagnitudes[index];
+            }
+
+            return seconds;
+        }
+        catch (Exception error)
+        {
+            throw new ArgumentException($"Failed to parse the timestamp '{timestamp}'", error);
+        }
+    }
+
+    protected static async Task<float> GetEffectiveDuration(ConfiguredTrack track)
+    {
+        if (track.VideoId == null) 
+            throw new NullReferenceException("Track VideoId must not be null.");
+        
+        var duration = await InfoCache.DurationCache.ComputeIfAbsentAsync(track.VideoId, FetchSongDuration);
+        var effectiveDuration = duration;
+
+        if (track.StartTimestamp is not null)
+        {
+            var startTime = ParseTimestamp(track.StartTimestamp);
+            if (duration <= startTime) return 0;
+
+            effectiveDuration -= startTime;
+        }
+
+        if (track.EndTimestamp is not null)
+        {
+            var endTime = ParseTimestamp(track.EndTimestamp);
+            if (duration <= endTime) return effectiveDuration;
+
+            effectiveDuration -= duration - endTime;
+        }
+
+        return effectiveDuration;
     }
 
     public static async Task DownloadTrack(ConfiguredTrack track)
@@ -40,10 +108,9 @@ public class TrackDownloader
             Logger?.LogDebug("Track already downloaded, nothing to do.");
             return;
         }
-        
-        var duration = await InfoCache.DurationCache.ComputeIfAbsentAsync(track.VideoId, FetchSongDuration);
+
+        var duration = await GetEffectiveDuration(track);
         if (duration > MaxSongDuration.Value)
-        {
             throw new VideoTooLongException("Track too long, skipping.");
         }
         
